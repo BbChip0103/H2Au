48cae6143605437e537890d7d73546f905693453
BbChip <qazwsx10000@naver.com>
2017-01-05 16:48:06 +0900

    Fix scenario

---
+++ Presentation/MathAndUs/mathgirls/Assets/UnityChan/Scripts/Fly.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour {
+
+	float height = 0.5f, speed = 0.01f;
+	int sw = 0, cnt = 0, cnt_limit = 60;
+	int i;
+
+	Vector3 move_vector_up = new Vector3 (0.0f, 1f, 0.0f);
+	Vector3 move_vector_down = new Vector3 (0.0f, -1f, 0.0f);
+	Vector3 move_vector_location = new Vector3 (0.7f, 0.0f, 0.0f);
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("UnityFly");
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	IEnumerator UnityFly(){
+		if (sw == 0) {
+			transform.position = move_vector_up * height * speed * cnt;
+			transform.position += move_vector_location;
+			if (cnt >= cnt_limit - 2) {
+				sw = 1;
+				cnt = 0;
+			} else {
+				cnt+=2;
+			}
+		} else {
+			transform.position = move_vector_down * height * speed * cnt;
+			transform.position += move_vector_up * cnt_limit * height * speed;
+			transform.position += move_vector_location;
+			if (cnt >= cnt_limit - 2) {
+				sw = 0;
+				cnt = 0;
+			} else {
+				cnt+=2;
+			}
+		}
+
+		yield return new WaitForSeconds(0.01f);
+
+		StartCoroutine ("UnityFly");
+	}
+}
---
+++ Presentation/MathAndUs/mathgirls/Assets/UnityChan/Scripts/FlyMath.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyMath : MonoBehaviour {
+
+	int cnt = 0, cnt_limit = 120;
+	int i;
+	float height, speed = 0.01f;
+
+	Vector3 move_vector = new Vector3 (0.0f, 1f, 0.0f);
+	Vector3 move_vector2 = new Vector3 (-0.7f, 0.0f, 0.0f);
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("UnityFly");
+		height = cnt_limit / 4;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	IEnumerator UnityFly(){
+
+		transform.position = move_vector * (1 - (Mathf.Cos(Mathf.PI * cnt * 2 / cnt_limit) / 2.0f + 0.5f)) * height * speed + move_vector2 ;
+		if (cnt >= cnt_limit - 2) {
+			cnt = 0;
+		}
+		else{
+			cnt+=2;
+		}
+
+		yield return new WaitForSeconds(0.01f);
+
+		StartCoroutine ("UnityFly");
+	}
+}
