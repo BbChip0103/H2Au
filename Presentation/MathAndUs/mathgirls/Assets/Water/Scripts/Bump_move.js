098398d21018b88f591d3f266e36e6bdb6641028
BbChip <qazwsx10000@naver.com>
2017-01-11 20:14:50 +0900

    Move presentation, practice file And Create new repository

--- Presentation/MathAndUs/mathgirls/Assets/Water/Scripts/Bump_move.js
+++
@@ -1,5 +0,0 @@
-﻿	// Scroll main texture based on time
-
-var scrollSpeed : float;
-
-function Update () { var offset : float = Time.time * scrollSpeed; GetComponent.<Renderer>().material.SetTextureOffset ("_BumpMap", Vector2(offset,0)); } 
